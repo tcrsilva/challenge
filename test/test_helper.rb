24ce76247650931dcf06f6dc74e83c061d2ea31b
Tais Silva <tais.crsilva@gmail.com>
2018-12-12 01:34:14 +0000

    last updates

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-
+require 'capivara/rails'
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
